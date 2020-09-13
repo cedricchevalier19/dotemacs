010cca969fa0825bff3e0f37260034c994d14dee
Cedric Chevalier <cedric.chevalier@cea.fr>
2020-09-13 22:03:19 +0200

    First init from https://github.com/caiohcs/my-emacs

---
+++ init.el
@@ -0,0 +1,29 @@
+(when (< emacs-major-version 27)
+  (setq gc-cons-threshold 100000000))
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(use-package diminish
+  :straight t
+  :defer t)
+(require 'bind-key)
+
+(use-package org
+  :straight (:type built-in))
+
+(use-package use-package-ensure-system-package
+  :straight t)
+
