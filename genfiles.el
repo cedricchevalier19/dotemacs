4df7fa67a9b01970722616910f3514f6f97d5358
Diego Zamboni <diego@zzamboni.org>
2021-07-07 10:30:32 +0200

    Generate AwesomeCV documents
    Both a CV and a cover letter are generated.
    
    All files are now processed with lualatex instead of pdflatex, since
    lualatex is needed for AwesomeCV and does not affect the others.

--- genfiles.el
+++ genfiles.el
@@ -10,13 +10,16 @@
 
 (require 'ox-altacv)
 
+(require 'ox-awesomecv)
+
 (let ((readme (concat cwd "readme.org")))
   (find-file readme)
   (make-directory workdir t)
   (cd workdir)
   (org-babel-tangle))
 
-(copy-file (concat cwd "doc/smile.png") workdir)
+(copy-file (concat cwd "doc/smile.png") workdir t)
+(copy-directory "/root/texmf/tex/latex/Awesome-CV-master/fonts" workdir)
 
 (defun export-latex (backend file)
   (let ((workfile (concat workdir file))
@@ -25,10 +28,12 @@
     (find-file workfile)
     (org-mode)
     (org-export-to-file backend outfile)
-    (shell-command (format "pdflatex %s" outfile) "*Messages*" "*Messages*")
-    (copy-file (concat file ".pdf") (concat cwd "/doc/static/" (concat file ".pdf")))
+    (shell-command (format "lualatex %s" outfile) "*Messages*" "*Messages*")
+    (copy-file (concat file ".pdf") (concat cwd "/doc/static/" (concat file ".pdf")) t)
     ))
 
 (make-directory (concat cwd "/doc/static/") t)
 (export-latex 'altacv "altacv.org")
 (export-latex 'moderncv "moderncv.org")
+(export-latex 'awesomecv "awesomecv.org")
+(export-latex 'awesomecv "awesome-letter.org")
