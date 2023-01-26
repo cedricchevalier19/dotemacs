dd4eab3c54a32bbde0d8c3ef712eacc796f5ac69
Cédric Chevalier <cedric.chevalier@cea.fr>
2023-01-26 19:20:12 +0100

    Update getting lsp-servers

--- lsp-servers.sh
+++ lsp-servers.sh
@@ -9,18 +9,21 @@ LSP_ROOT_TOOLS=~/opt/lsp-tools
 mkdir -p $LSP_ROOT_TOOLS/bin
 
 # markdown
+echo "Getting markdown"
 if [ ! -x "$(readlink -f ${LSP_ROOT_TOOLS}/bin/marksman)" ]; then
     cd ${LSP_ROOT_TOOLS} || exit
     curl -L https://github.com/artempyanykh/marksman/releases/download/2022-10-30/marksman-linux -o bin/marksman && chmod +x bin/marksman;
 fi
 
 # docker
+echo "Getting docker"
 cd ${LSP_ROOT_TOOLS} || exit
 npm install --prefix ${LSP_ROOT_TOOLS} dockerfile-language-server-nodejs
 cd bin || exit
 ln -sf ../node_modules/dockerfile-language-server-nodejs/bin/docker-langserver .
 
 # cmake
+echo "Getting cmake"
 cd ${LSP_ROOT_TOOLS} || exit
 pip install --prefix=${LSP_ROOT_TOOLS} cmake-language-server
 if [ ! -x bin/cmake-language-server ]; then
@@ -30,12 +33,14 @@ if [ ! -x bin/cmake-language-server ]; then
 fi
 
 #bash
+echo "Getting bash"
 cd ${LSP_ROOT_TOOLS} || exit
 npm install --prefix ${LSP_ROOT_TOOLS} bash-language-server
 cd bin || exit
 ln -sf ../node_modules/bash-language-server/bin/main.js bash-language-server
 
 # bash too
+echo "Getting shellcheck"
 if [ ! -x "$(readlink -f ${LSP_ROOT_TOOLS}/bin/shellcheck)" ]; then
     cd ${LSP_ROOT_TOOLS} || exit
     mkdir -p ${LSP_ROOT_TOOLS}/shellcheck
@@ -46,13 +51,14 @@ if [ ! -x "$(readlink -f ${LSP_ROOT_TOOLS}/bin/shellcheck)" ]; then
 fi
 
 #clangd
-if [ ! -x "$(readlink -f ${LSP_ROOT_TOOLS}/bin/clangd-indexer)" ]; then
+echo "Getting clangd"
+if [ ! -x "$(readlink -f ${LSP_ROOT_TOOLS}/bin/clangd)" ]; then
     cd ${LSP_ROOT_TOOLS} || exit
     mkdir -p ${LSP_ROOT_TOOLS}/clangd
-    CLANGD_RELEASE=15.0.3
-    curl -L https://github.com/clangd/clangd/releases/download/${CLANGD_RELEASE}/clangd_indexing_tools-linux-${CLANGD_RELEASE}.zip | bsdtar x -C ${LSP_ROOT_TOOLS}/clangd --strip-components=1
+    CLANGD_RELEASE=15.0.6
+    curl -L https://github.com/clangd/clangd/releases/download/${CLANGD_RELEASE}/clangd-linux-${CLANGD_RELEASE}.zip | bsdtar x -C ${LSP_ROOT_TOOLS}/clangd --strip-components=1
     cd bin || exit
-    for exe in ../clangd/bin/clangd-* ; do
+    for exe in ../clangd/bin/clangd* ; do
         chmod +x "${exe}"
         ln -sf "${exe}" .;
     done;
