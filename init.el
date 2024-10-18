5719102cd8bef4a131aef15ab877d7cedfae14e4
Cédric Chevalier <cedric.chevalier019@proton.me>
2024-10-18 22:50:06 +0200

    Email (#44)
    * Email parameter
    
    * Fix a typo

--- init.el
+++ init.el
@@ -10,11 +10,16 @@
 
 (defconst config-org (locate-user-emacs-file "readme.org"))
 (defconst config-el (locate-user-emacs-file "config.el"))
+(defconst config-email-el (locate-user-emacs-file "local/email.el"))
 
 (unless (file-exists-p config-el)
   (require 'org)
   (org-babel-tangle-file config-org config-el "emacs-lisp"))
 
 (load-file config-el)
+
+(if (file-exists-p config-email-el)
+    (load-file config-email-el))
+
 (provide 'init)
 ;;; init.el ends here
